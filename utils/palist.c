e80b06ddb2c17ad46ec1b667a81f88da65877e20
Paul W. Frields <stickster@gmail.com>
2009-05-24 21:03:31 -0400

    Fix header and description in palist.c

--- utils/palist.c
+++ utils/palist.c
@@ -1,10 +1,24 @@
 /*
- * palist.c - List sinks and/or sources available on a PulseAudio server
+ * palist.c - List sinks and sources from the local PulseAudio server
  *
- * This file is licensed under terms of the GNU General Public License
- * (GPL), either version 2 or (at your option) any later version.  No
- * warranty is herein expressed or implied.  Refer to the GNU GPL for
- * more details.
+ * Copyright (C) 2009 Paul W. Frields
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ *
+ * Author: Paul W. Frields <stickster@gmail.com>
  *
  */
 
