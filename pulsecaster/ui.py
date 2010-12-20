a7b2271a969a06d639fb0ac27d4a22b797ba4946
Paul W. Frields <stickster@gmail.com>
2010-12-20 17:31:16 -0500

    Put app name on warning dialog

--- pulsecaster/ui.py
+++ pulsecaster/ui.py
@@ -71,6 +71,7 @@ class PulseCasterUI:
         self.swckbox.set_active(int(self.gconfig.skip_warn))
         self.dismiss.connect('clicked', self.hideWarn)
         self.warning.connect('destroy', self.on_close)
+        self.warning.set_title(NAME)
         
         # Main dialog basics
         self.main = self.builder.get_object('main_dialog')
