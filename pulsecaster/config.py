95c9fad911714584988142323b15a17a4b7e78e0
Paul W. Frields <stickster@gmail.com>
2019-09-03 18:38:48 -0400

    Fix GIO_PATH

--- pulsecaster/config.py
+++ pulsecaster/config.py
@@ -61,5 +61,5 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 02110-1301, USA.'''
 
-GIO_PATH='/'.join(('org', LNAME, NAME))
+GIO_PATH='/org/%s/%s/' % (LNAME, NAME)
 GIO_ID='.'.join(('org', LNAME, NAME))
